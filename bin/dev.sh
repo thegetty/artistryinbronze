cd2deb0a58f332bde7ca7c1f3297c7437c7ded4c
Eric Gardner <gardner.ec@gmail.com>
2017-01-12 10:44:39 -0800

    Update content

---
+++ bin/dev.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Load variables from the config file
+CONFIG="./bin/config.cfg"
+source $CONFIG
+
+# Run the Hugo and Gulp concurrently
+trap "kill %1" EXIT
+# hugo server & cd "themes/$THEME" && ./node_modules/.bin/webpack --watch
+cd "themes/$THEME" && ./node_modules/.bin/webpack --watch & cd "../.." && hugo server
---
+++ bin/setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Load variables from the config file
+CONFIG="./bin/config.cfg"
+source $CONFIG
+
+# Check if Hugo is installed
+command -v hugo >/dev/null 2>&1 || {
+    echo >&2 "Please install Hugo before proceeding: https://gohugo.io/"; exit 0;
+}
+
+# Check if node is installed
+command -v node >/dev/null 2>&1 || {
+    echo >&2 "Please install Node.js before proceeding."; exit 0;
+}
+
+# Install the theme if it hasn't been downloaded already
+if [ ! -d "./themes/$THEME" ]; then
+    cd themes && git clone $THEME_URL
+    cd ..
+fi
+
+# Install theme dependencies
+cd "themes/$THEME" && npm install
+
+# Confirmation message
+echo "Theme successfully installed. Run bin/dev to view local changes."
