d31b9e40b840a4407a1e878c88c4a5c3cba9e470
Eric Gardner <gardner.ec@gmail.com>
2017-01-17 15:44:44 -0800

    Fix the shell scripts for new theme
    To run Webpack and Hugo at the same time, just run ./bin/dev.sh in the
    project root.

--- bin/dev.sh
+++ bin/dev.sh
@@ -6,5 +6,5 @@ source $CONFIG
 
 # Run the Hugo and Gulp concurrently
 trap "kill %1" EXIT
-# hugo server & cd "themes/$THEME" && ./node_modules/.bin/webpack --watch
-cd "themes/$THEME" && ./node_modules/.bin/webpack --watch & cd "../.." && hugo server
+hugo server & cd "themes/$THEME" && ./node_modules/.bin/webpack --watch
+# cd "themes/$THEME" && ./node_modules/.bin/webpack --watch & cd "../.." && hugo server
