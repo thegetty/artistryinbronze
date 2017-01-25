6e705bfcb3c75958d4c81318fd6174fc96b5ac0f
Eric Gardner <gardner.ec@gmail.com>
2017-01-24 18:24:49 -0800

    Improve CLI workflow with npm scripts
    At this point the structure of this starter package is finally taking
    shape. It looks like the best solution for now is to use Gulp as a
    mini-cli (with tasks invoked through NPM scripts). This should impose
    the least amount of cognitive overhead on the user. All they have to do
    is:
    
    1. Make sure they have hugo and git installed on their system
    2. Download the git repo for the starter package
    2a (optional). Run `git remote rm origin` and replace with their own
    3. Run `npm install` inside the project folder
    4. Run `npm run setup`, `npm run dev`, and `npm run build` as needed.

--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,108 @@
-const gulp = require('gulp')
+const chalk = require('chalk')
 const exec = require('child_process').exec
-const spawn = require('child_process').spawn
+const exists = require('command-exists')
+const fs = require('fs')
+const gulp = require('gulp')
 const path = require('path')
+const spawn = require('child_process').spawn
+const spawnSync = require('child_process').spawnSync
+const yaml = require('js-yaml')
 
-const THEME_PATH = path.join(__dirname, 'themes', 'quire-catalogue-theme')
+const THEME_NAME = yaml.safeLoad(fs.readFileSync('./config.yml', 'utf8')).theme
+const THEME_PATH = path.join(__dirname, 'themes', THEME_NAME)
 const WEBPACK_BIN = './node_modules/.bin/webpack'
 
+// Tasks ///////////////////////////////////////////////////////////////////////
+//
+// Setup (run on first install only)
+gulp.task('setup', ['check dependencies', 'install theme'])
+
+// Ensure that Hugo is installed
+gulp.task('check dependencies', (cb) => {
+  exists('hugo', (err, commandExists) => {
+    if (commandExists) {
+      console.log(chalk.green('Hugo is ready'))
+    } else {
+      return console.log(
+        chalk.yellow('Please install Hugo before proceeding \n') +
+          'Run `brew install hugo` if using homebrew \n' +
+          'Or visit ' + chalk.underline('http://gohugo.io/') +
+          ' for installation instructions.'
+      )
+    }
+    cb(err)
+  })
+})
+
+// Install the theme
+gulp.task('install theme', ['check dependencies'], () => {
+  if (fs.existsSync(THEME_PATH)) {
+    console.log(chalk.green('Theme is installed'))
+    console.log('Type `npm run preview` to view local changes.')
+  } else {
+    downloadTheme(THEME_NAME)
+    installDependencies(THEME_PATH)
+    console.log(chalk.green('Theme successfully installed'))
+  }
+})
+
 // Run webpack in the themes folder
-gulp.task('theme', function(cb) {
-  exec('./node_modules/.bin/webpack', { cwd: THEME_PATH }, function(err) {
+gulp.task('theme', (cb) => {
+  console.log('Rebuilding theme files..')
+  exec('./node_modules/.bin/webpack', { cwd: THEME_PATH }, (err) => {
     if (err) return cb(err)
     cb()
+    console.log(chalk.green('Theme built successfully'))
   })
 })
 
 // Run the Hugo build to see content changes
-gulp.task('content', function(cb) {
-  exec('hugo', function(err) {
+gulp.task('content', (cb) => {
+  exec('hugo', (err) => {
     if (err) return cb(err)
     cb()
   })
 })
 
 // Run the Hugo build task with webpack as a prereq
-gulp.task('build', ['theme'], function(cb) {
-  exec('hugo', function(err) {
+gulp.task('build', ['theme'], (cb) => {
+  console.log('Rebuilding content files...')
+  exec('hugo', (err) => {
+    if (err) return cb(err)
+    cb()
+    console.log(chalk.green('Content files built succesfully.'))
+    console.log(chalk.bold.green('Site built in public/'))
+  })
+})
+
+// Run hugo server and webpack --watch simultaneously, with merged output
+gulp.task('dev', (cb) => {
+  spawn(WEBPACK_BIN, ['--watch'], { cwd: THEME_PATH, stdio: 'inherit' }, (err) => {
+    if (err) return cb(err)
+    cb()
+  })
+
+  spawn('hugo', ['server'], { stdio: 'inherit' }, (err) => {
     if (err) return cb(err)
     cb()
   })
 })
 
+// Helper Functions ////////////////////////////////////////////////////////////
 //
-gulp.task('dev', function(cb) {
-  spawn(WEBPACK_BIN, ['--watch'], { cwd: THEME_PATH, stdio: 'inherit' }, function(err) {
-    if (err) return cb(err)
-    cb()
-  })
+function downloadTheme(themeName) {
+  let themeUrl = `https://github.com/gettypubs/${themeName}`
+  spawnSync('git', ['clone', themeUrl], {
+    cwd: path.join(__dirname, 'themes'),
+    stdio: 'inherit'
+  },
+    function(err) { if (err) return console.log(err) }
+  )
+}
 
-  spawn('hugo', ['server'], { stdio: 'inherit' }, function(err) {
-    if (err) return cb(err)
-    cb()
-  })
-
-  // This works but there is no output in the console
-  // exec('hugo server', function(err, stdout, stderr) {
-    // if (err) return cb(err)
-    // console.log(stdout) // See Hugo output
-    // console.log(stderr) // Debugging feedback
-    // cb()
-  // })
-  // exec('./node_modules/.bin/webpack --watch', { cwd: THEME_PATH }, function(err, stdout, stderr) {
-    // if (err) return cb(err)
-    // console.log(stdout) // See Hugo output
-    // console.log(stderr) // Debugging feedback
-    // cb()
-  // })
-})
+function installDependencies(dir) {
+  spawnSync('npm', ['install'], { cwd: dir, stdio: 'inherit' },
+    function(err) { if (err) return console.log(err) }
+  )
+}
 
